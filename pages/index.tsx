1c7f930695794b1f95645b7c22f1aba09c240041
Sabin Adams <sabintheworld@gmail.com>
2022-09-03 23:54:40 -0700

    Started layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,21 +4,24 @@ import ThemeToggler from '../components/ThemeToggler'
 
 const Home: NextPage = () => {
   return (
-    <div className="max-h-full min-h-screen px-4 mx-auto transition duration-300 ease-in-out dark:bg-slate-700">
+    <div className="relative max-h-full min-h-screen px-8 mx-auto transition duration-300 ease-in-out dark:bg-slate-700">
       <Head>
-        <title>ORMs Explained</title>
+        <title>The one about ORMs</title>
         <meta name="description" content="A look at why you should use an ORM, the history behind them, and the ORMs currently available." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="pt-4 px-4 mx-auto">
+      <header className="pt-4 px-4 mx-auto absolute top-0 w-screen left-0">
         <div className="flex justify-end">
           <ThemeToggler/>
         </div>
       </header>
 
-      <main>
-        <h1 className="text-4xl font-extrabold">ORMs</h1>
+      <main className="text-xl">
+        <div className="text-center h-screen flex flex-col items-center justify-center">
+          <h1 className="text-5xl font-extrabold">ORMs</h1>
+          <p>Why you should use them, how you should use them, and what your options are.</p>
+        </div>
       </main>
 
       <footer>
